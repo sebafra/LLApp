c335f9d3fe58f8d2be5300926f5db6c4628d0982
Seba Fraresso <seba@diproach.com>
2014-06-12 12:34:36 -0300

    Ajustes 2

--- app/App.js
+++ app/App.js
@@ -7,7 +7,7 @@ window.App = {
 	worksLocal:function(){
 		return true;
 	},
-	
+
 	querys:["uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve","diez","once","doce","trece","catorce","quince"],
 	querysAnswers:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 	queryIdx:0,
